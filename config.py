25eb9f25901b4bd636107e4b3455986a7d11695a
Tohirov Abdulloh <tohirovsarvar219@gmail.com>
2026-04-12 23:19:17 +0500

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-BOT_TOKEN    = "8748024625:AAGxIsHhAt6NVRervI_bQ_ZF_thEIqB4CCY"       # @BotFather dan oling
-ADMIN_ID     = 8321214528                   # Sizning Telegram ID
-MINI_APP_URL = "https://dilshod-kafe-app.vercel.app/"  # Vercelga yuklagandan keyin
-
-# JSONBin.io (bepul) — mahsulotlarni saqlash uchun
-# https://jsonbin.io ga ro'yxatdan o'ting, bin yarating
-JSONBIN_ID   = "69dbd80b856a682189277408"
-JSONBIN_KEY  = "$2a$10$4mRVG0CBGI7s50wk0eCdQO8r4lqu21iyX4dvLS/R7p/aXUA6TvRJC"
